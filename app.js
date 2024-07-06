56175a3e07ceb51a78f2a16bad6812e1eb358c95
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-07-06 19:47:16 +0100

    my first commit for backend tuts

---
+++ app.js
@@ -0,0 +1,80 @@
+const Joi = require("joi");
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const courses = [
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" },
+];
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.get("/api/courses", (req, res) => {
+  res.send(courses);
+});
+
+// get request with an id
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) {
+    res.status(404).send("the course you are looking for does not exist");
+  } else {
+    res.send(course);
+  }
+});
+
+// post request
+app.post("/api/courses", (req, res) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+
+  const { error } = schema.validate(req.body);
+
+  if (error) return res.status(404).send(error.details[0].message);
+
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
+  res.send(course);
+});
+
+// put request
+app.put("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    return res
+      .status(404)
+      .send("This course you are looking for does not exist");
+
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+  const { error } = schema.validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  course.name = req.body.name;
+  res.send(course);
+});
+
+// delete request
+app.delete("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    res.status(404).send("The course you are looking for does not exist");
+
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+
+  res.send(course);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`server is running on port ${port}...`));
