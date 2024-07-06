8800a8f16207391d0c2bc37aa773e68e3f9701b3
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-07-06 19:52:28 +0100

    added more courses to my api

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const courses = [
   { id: 1, name: "course1" },
   { id: 2, name: "course2" },
   { id: 3, name: "course3" },
+  { id: 4, name: "course4" },
+  { id: 5, name: "course5" },
 ];
 
 app.get("/", (req, res) => {
